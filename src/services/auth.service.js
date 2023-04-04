54591d4955308a486bd0035bb121a0cdd87cb1aa
pedro josé lima <pedrlimadev@gmail.com>
2023-04-04 10:56:52 -0300

    JWT Added

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,6 +1,10 @@
 import User from "../models/User.js";
+import jwt from "jsonwebtoken";
+
+const generateToken = (id) =>
+    jwt.sign({ id: id }, process.env.SECRET_JWT, { expiresIn: 86400 });
 
 const loginService = (username) =>
     User.findOne({ username: username }).select("+password");
 
-export { loginService };
+export { loginService, generateToken };
