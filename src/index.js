870f4dcb61e256cf2db3e4e910118abd874fa182
pedro josé lima <pedrlimadev@gmail.com>
2023-04-19 21:02:17 -0300

    Swagger UI Documentation (Added)

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import express from "express";
-import connectDatabase from "./src/database/db.js";
+import connectDatabase from "./database/db.js";
 import dotenv from "dotenv";
 dotenv.config();
 
-import userRoute from "./src/routes/user.route.js";
-import authRoute from "./src/routes/auth.route.js";
-import newsRoute from "./src/routes/news.route.js";
+import userRoute from "./routes/user.route.js";
+import authRoute from "./routes/auth.route.js";
+import newsRoute from "./routes/news.route.js";
+import swaggerRoute from "./routes/swagger.route.cjs";
 
 const port = process.env.PORT || 3000;
 const app = express();
@@ -16,5 +17,6 @@ app.use(express.json());
 app.use("/user", userRoute);
 app.use("/auth", authRoute);
 app.use("/news", newsRoute);
+app.use("/doc", swaggerRoute);
 
 app.listen(port, () => console.log(`SERVER RUNNING ON ${port} PORT!`));
