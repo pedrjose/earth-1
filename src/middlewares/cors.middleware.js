bb8979c07132d9ec636fc8a0b1692c81373956d0
pedro josé lima <pedrlimadev@gmail.com>
2023-06-06 10:35:17 -0300

    Add CORS Header Settings

--- src/middlewares/cors.middleware.js
+++ src/middlewares/cors.middleware.js
@@ -1,8 +1,9 @@
 export const corsAuth = (req, res, next) => {
     try {
+        // 'Content-Type'
         res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
         res.header('Access-Control-Allow-Methods', ['*']);
-        res.header('Access-Control-Allow-Headers', 'Content-Type');
+        res.header('Access-Control-Allow-Headers', ['*']);
         next();
     } catch (err) {
         res.status(500).send({ message: err.message });
