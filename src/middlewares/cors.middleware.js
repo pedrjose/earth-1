ea5c88f547aee641ad3f10e5382703c99f5c950f
pedro josé lima <pedrlimadev@gmail.com>
2023-06-07 10:36:40 -0300

    edit cors #2

--- src/middlewares/cors.middleware.js
+++ src/middlewares/cors.middleware.js
@@ -2,7 +2,7 @@ export const corsAuth = (req, res, next) => {
     try {
         res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
         res.header('Access-Control-Allow-Methods', ['*']);
-        res.header('Access-Control-Allow-Headers', ['Authorization']);
+        res.header('Access-Control-Allow-Headers', ['*']);
         next();
     } catch (err) {
         res.status(500).send({ message: err.message });
